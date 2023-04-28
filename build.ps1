fe52c16175a2e01aecfe47468a046fa2a9c05e1c
niltor <zpty@outlook.com>
2023-04-28 15:47:18 +0800

    try multi language support

--- build.ps1
+++ build.ps1
@@ -1,5 +1,23 @@
 Remove-Item .\_site -Recurse -Force
-DocFxTocGenerator -d ./en
-DocFxTocGenerator -d ./zh
-docfx build
+DocFxTocGenerator -d ./zh/ater.web  -r
+DocFxTocGenerator -d ./zh/droplet  -r
+
+# docfx build
+
+# 中文
+docfx build .\zh\docfx-zh.json
+# Move-Item .\_site\zh-tmp\zh .\_site\zh
+# Remove-Item .\_site\zh-tmp\ -Recurse -Force
+
+
+# 英文
+docfx build .\en\docfx-en.json
+# Move-Item .\_site\en-tmp\en .\_site\en
+# Remove-Item .\_site\en-tmp\ -Recurse -Force
+
+
+Remove-Item .\_site\zh\styles\*.map -Force
+
+Copy-Item .\src\index.html .\_site\
+# Remove-Item .\_site\en\styles\*.map -Force
 docfx serve
