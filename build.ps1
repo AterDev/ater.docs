c173605238adab7f07dd35c401bbcdcaeff898c9
niltor <zpty@outlook.com>
2023-05-04 18:44:42 +0800

    更新文档toc

--- build.ps1
+++ build.ps1
@@ -1,23 +1,16 @@
 Remove-Item .\_site -Recurse -Force
-DocFxTocGenerator -d ./zh/ater.web  -r
-DocFxTocGenerator -d ./zh/droplet  -r
+# generate TOC
+DocFxTocGenerator -d ./zh/ater.web  -r -g
+DocFxTocGenerator -d ./zh/droplet  -r -g
+
+DocFxTocGenerator -d ./en/ater.web  -r -g
+DocFxTocGenerator -d ./en/droplet  -r -g
 
 # docfx build
-
-# 中文
 docfx build .\zh\docfx-zh.json
-# Move-Item .\_site\zh-tmp\zh .\_site\zh
-# Remove-Item .\_site\zh-tmp\ -Recurse -Force
-
-
-# 英文
 docfx build .\en\docfx-en.json
-# Move-Item .\_site\en-tmp\en .\_site\en
-# Remove-Item .\_site\en-tmp\ -Recurse -Force
-
 
 Remove-Item .\_site\zh\styles\*.map -Force
+Remove-Item .\_site\en\styles\*.map -Force
 
 Copy-Item .\src\index.html .\_site\
-# Remove-Item .\_site\en\styles\*.map -Force
-docfx serve
